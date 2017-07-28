667371aa9f052d11d6ca26dcd2b6fc9b303ae681
Ryan Kiang <ryanlkiang@gmail.com>
2017-07-28 14:00:53 -0500

    added button to html and click event to js

--- scripts.js
+++ scripts.js
@@ -7,5 +7,11 @@ $(document).ready(function() {
     $('#kevin li').css('color', 'maroon');
     $('.food').css('color', 'pink');
     $('#chief').css('color', 'green');
-    $('li:last-child').css('color', 'black');
+    // $('#kevin li:last-child').css('color', 'black'); //$('ul:first li:last-child) works the same
+    $('ul').first().children().last().css('color', 'black'); //done with methods of jQuery selector
+
+    $('#kevinNewFavorite').on('click', function(){
+        console.log('button was clicked');
+        
+    });
 });
